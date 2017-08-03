15fefc2b41eea4290ed26f2204bb7795c678fdf6
bimatn <bimatn@outlook.com>
2017-08-02 17:39:01 -0700

    have a working game but need an end still

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,11 +3,13 @@
 //----------------------VARIABLE SECTION-------------------------------------//
 
 	var currentWord;
+	var rightCount;
 	var wins=0;
 	var losses=0;
 	var playAgain;
+	var nextWord=0;
 
-//------------------------------------------------OBJECT SECTION FOR WORDS-------------------------------------------------//
+//------------------------------------------------SECTION FOR WORDS OBJECTS-------------------------------------------------//
 
 
 	var gameWords = [
@@ -17,7 +19,8 @@
 				letters: ["T", "R", "O", "N"],
 				guesses: [],
 				wordDisplay: ["_","_","_","_"],
-				guessCount: 6
+				guessCount: 6,
+				correctCount: 4
 			},
 
 		word2= 
@@ -26,7 +29,8 @@
 				letters: ["H", "O", "O", "K"],
 				guesses: [],
 				wordDisplay: ["_ ","_ ","_ ","_"],
-				guessCount: 6
+				guessCount: 6,
+				correctCount: 4
 			},
 
 		word3= 
@@ -34,8 +38,9 @@
 				clue: "\"No one can be told what the Matrix is.\"",
 				letters: ["M", "O", "R", "P", "H", "E", "U", "S"],
 				guesses: [],
-				wordDisplay: ["_ ","_ ","_ ","_ ","_ ","_ ","_ ","_"],
-				guessCount: 8
+				wordDisplay: ["_","_","_","_","_","_","_","_"],
+				guessCount: 8,
+				correctCount: 8
 			},
 
 		word4=
@@ -43,8 +48,9 @@
 				clue: "\"Tell him to stay, Murph. Tell him to stay!\"",
 				letters: ["I", "N", "T", "E", "R", "S", "T", "E", "L", "L", "A", "R"],
 				guesses: [],
-				wordDisplay: ["_ ","_ ","_ ","_ ","_ ","_ ","_ ","_ ","_ ","_ ","_ ","_"],
-				guessCount: 8
+				wordDisplay: ["_","_","_","_","_","_","_","_","_","_","_","_"],
+				guessCount: 8,
+				correctCount: 12
 			},
 
 		word5=
@@ -52,12 +58,34 @@
 				clue: "\"They're taking the Hobbits to Isengard!\"",
 				letters: ["L", "E", "G", "O", "L", "A", "S"],
 				guesses: [],
-				wordDisplay: ["_ ","_ ","_ ","_ ","_ ","_ ","_ "],
-				guessCount: 8
+				wordDisplay: ["_","_","_","_","_","_","_"],
+				guessCount: 8,
+				correctCount: 7
 			}
 		]
 
 //---------------------------------------------------------------FUNCTION SECTION----------------------------------------------------------------------------//
+
+//Function to play again or reset
+
+	function playAgainResponse()
+	{
+		if(playAgain === true)
+		{
+			console.log("user wants to play again");
+			nextWord++;
+			rightCount=0;
+			currentWord=gameWords[nextWord];
+			document.getElementById("wordClue").innerHTML = "Word Clue: "+currentWord.clue; //Shows word clue to user
+			document.getElementById("wordDisplay").innerHTML = "Word: "+ convertAndReplace(currentWord.wordDisplay); //shows word character spaces to user
+			document.getElementById("userGuessDisplay").innerHTML = "Letters Guessed: "+convertAndReplace(currentWord.guesses);
+			document.getElementById("guessCount").innerHTML = "Incorrect Guesses Left: "+currentWord.guessCount; // shows remaining guess count to user
+		}
+		else
+		{
+			location.reload(); // refreshes page to very beginning
+		}
+	}
 
 //Function to convert array to string and replace commas of string with spaces and then return the new string
 	function convertAndReplace(array) 
@@ -72,11 +100,17 @@
 
 	function winCheck() 
 	{
-		if(convertAndReplace(currentWord.wordDisplay) === convertAndReplace(currentWord.letters)) //test to see if word has been guessed
+		if(rightCount===currentWord.correctCount) //test to see if word has been guessed
 		{
-			wins++; //word has been guessed, increase wins
+			//console.log(rightCount);
+			//console.log(currentWord.correctCount);
+			console.log("running you have won response");
+			wins=wins+1; //word has been guessed, increase wins
+			console.log("win count: "+wins);
+			console.log("should have 0 losses: " + losses);
 			document.getElementById("wins").innerHTML = "Wins: "+ wins; // shows win count to user
 			playAgain = confirm("You have won! Great job! Now go change the world! Or perhaps you'd like to play again?"); //asks user to play again
+			playAgainResponse(); //test play again response
 		}
 	}
 
@@ -88,24 +122,8 @@
 		{
 			losses++;
 			document.getElementById("losses").innerHTML = "Losses: "+ losses; // shows win count to user
-			playAgain = confirm("You have lost! Go watch more movies! Or if you dare, would you like to play again?");
-		}
-	}
-
-//Function for comparing guess to previous guesses
-	
-	function prevGuess(guess)
-	{
-		if (currentWord.guesses.indexOf(guess) === -1) //comparing guess with previous guesses array
-		{
-			currentWord.guesses.push(guess); //Add guess to guesses array
-			document.getElementById("userGuessDisplay").innerHTML = "Letters Guessed: "+convertAndReplace(currentWord.guesses); //shows array of user guesses
-			document.getElementById("guessForm").reset(); //clears form after text entry
-		}
-		else //Runs if you have already guessed that letter
-		{
-			document.getElementById("guessForm").reset();//clears form after text entry
-			alert("You have already guessed this letter. Pay attention!"); //Tell user to try again
+			playAgain = confirm("You have lost! Go watch more movies! Or if you dare, would you like to play again?"); //asks user to play again
+			playAgainResponse(); //test play again response
 		}
 	}
 
@@ -113,29 +131,47 @@
 
 	function actionTime(guess)
 	{
-		for(i=0; i<currentWord.letters.length; i++) //searching through currentWord array one character at a time
+		if (currentWord.guesses.indexOf(guess) === -1) //comparing guess with previous guesses array
 		{
-			if(currentWord.letters[i] === guess) //testing for matches
+			currentWord.guesses.push(guess); //Add guess to guesses array
+			document.getElementById("userGuessDisplay").innerHTML = "Letters Guessed: "+convertAndReplace(currentWord.guesses); //shows array of user guesses
+			document.getElementById("guessForm").reset(); //clears form after text entry
+
+			for(i=0; i<currentWord.letters.length; i++) //searching through currentWord array one character at a time
 			{
-				for(j=i;j<currentWord.letters.length; j++) //since it matches we run test w/o decreasing guess count but still catching all matches
+				if(currentWord.letters[i] === guess) //testing for matches
 				{
-					if(currentWord.letters[j] === guess) //testing for matches
+					console.log("this is i:"+i);
+					for(j=i;j<currentWord.letters.length; j++) //since it matches we run test w/o decreasing guess count but still catching all matches
 					{
-						currentWord.wordDisplay.splice(j,1,guess);	//since it is a match we will replace the correct wordDisplay character with guess
-						document.getElementById("wordDisplay").innerHTML = "Word: "+ convertAndReplace(currentWord.wordDisplay); //we then update the wordDisplay
-
-						setTimeout(function(){ winCheck(); }, 200); //run win check
+						console.log("this is i:"+i);
+						console.log("this is j:"+j);
+						if(currentWord.letters[j] === guess) //testing for matches
+						{
+							currentWord.wordDisplay.splice(j,1,guess);	//since it is a match we will replace the correct wordDisplay character with guess
+							document.getElementById("wordDisplay").innerHTML = "Word: "+ convertAndReplace(currentWord.wordDisplay); //we then update the wordDisplay
+							rightCount++;
+							console.log("I'm about to run the win test");
+							winCheck();
+							//setTimeout(function(){ winCheck(); }, 1000); //causes issues
+							console.log("I ran it");
+						}
 					}
+					break; //break out of higher for loop to prevent else if for non match characters and to prevent repeat checks
 				}
-				break; //break out of higher for loop to prevent else if for non match characters and to prevent repeat checks
+				else if(i === (currentWord.letters.length-1)) //if it is not a match I want to take away a guess, but only one per wrong guess not per character
+				{
+					currentWord.guessCount--; //decreases guess count
+					document.getElementById("guessCount").innerHTML = "Guesses Left: "+currentWord.guessCount; //updates guess count display
+					lossCheck();
+					//setTimeout(function(){ lossCheck(); }, 200); //causes issues
+				}
 			}
-			else if(i === (currentWord.letters.length-1)) //if it is not a match I want to take away a guess, but only one per wrong guess not per character
-			{
-				currentWord.guessCount--; //decreases guess count
-				document.getElementById("guessCount").innerHTML = "Guesses Left: "+currentWord.guessCount; //updates guess count display
-
-				setTimeout(function(){ lossCheck(); }, 200); //run loss check
-			}
+		}
+		else //Runs if you have already guessed that letter
+		{
+			document.getElementById("guessForm").reset();//clears form after text entry
+			alert("You have already guessed this letter. Pay attention!"); //Tell user to try again
 		}
 	}
 
@@ -145,48 +181,21 @@
 		var guess=event.which || event.keyCode; //to account for browswer cross compatibility and assign character code to variable
 		var guess = String.fromCharCode(guess); //to convert character code to letter and assign to variable
 		guess=guess.toUpperCase(); //change guess to uppercase
-		prevGuess(guess); //is it a previous guess?
 		actionTime(guess);	//process guess to either be good, bad, win, or lose
 	}
 
 
-//----STARTING FUNCTION----Function for starting and playing game - relies on the start button press
+//----STARTING FUNCTION----Function for starting and playing game - relies on the start button press or play again = true
 //Prepare yourselves, for glory!
 
 	function hangmanPlay()
-	{						
-		//Random Word Choice for Hangman
-
-		ranSel=Math.floor(Math.random()*100);
-
-		if(ranSel <= 20) 
-		{
-			currentWord=word1;
-		}
-
-		else if (ranSel <= 40) 
-		{
-			currentWord=word2;
-		}
-
-		else if (ranSel <= 60) 
-		{
-			currentWord=word3;
-		}
-
-		else if (ranSel <= 80) 
-		{
-			currentWord=word4;
-		}
-
-		else
-		{
-			currentWord=word5;
-		}
-
+	{	
+		currentWord=gameWords[0]; //set game word
+		rightCount=0; //set right count to 0					
 		document.getElementById("wordClue").innerHTML = "Word Clue: "+currentWord.clue; //Shows word clue to user
 		document.getElementById("wordDisplay").innerHTML = "Word: "+ convertAndReplace(currentWord.wordDisplay); //shows word character spaces to user
-		document.getElementById("guessCount").innerHTML = "Incorrect Guesses Left: "+currentWord.guessCount; // shows guess count to user
+		document.getElementById("userGuessDisplay").innerHTML = "Letters Guessed: "+convertAndReplace(currentWord.guesses);
+		document.getElementById("guessCount").innerHTML = "Incorrect Guesses Left: "+currentWord.guessCount; // shows remaining guess count to user
 		document.getElementById("wins").innerHTML = "Wins: "+ wins; // shows win count to user
 		document.getElementById("losses").innerHTML = "Losses: "+ wins; // shows loss count to user
 		document.getElementById("userGuess").addEventListener("keyup", function(){ analyze(event);}); // processes user guess
